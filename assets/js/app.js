85078003407f4dc9762a84d7ade257ced14ac532
michellechristina <michelle@thisisbam.com>
2017-11-04 20:46:29 -0400

    fixed formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,14 +18,12 @@ $("#addTrainBtn").on("click", function () {
 
   // Grabs user input and assign to variables
   var trainName = $("#trainNameInput").val().trim();
-  // var lineName = $("#lineInput").val().trim();
   var destination = $("#destinationInput").val().trim();
   var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
   var frequencyInput = $("#frequencyInput").val().trim();
 
   // Test for variables entered
   console.log(trainName);
-
   console.log(destination);
   console.log(trainTimeInput);
   console.log(frequencyInput);
